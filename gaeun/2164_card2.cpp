a965ca54970754b914a21a94587d4e03966cb335
ghan <hge427568@gmail.com>
2024-03-26 20:39:16 +0900

    2nd done

--- gaeun/2164_card2.cpp
+++ gaeun/2164_card2.cpp
@@ -4,17 +4,19 @@ using namespace std;
 
 int main(){
     int cardNum;
+    int goBehindNum;
     queue<int> qCard;
 
     cin >> cardNum;
     for(int i=1; i<= cardNum; i++){
         qCard.push(i);
     }
-    for(int i=1; i<= cardNum; i++){
+    for(int i=1; i<cardNum; i++){
         qCard.pop();
-        qCard.pop();
-        qCard.push(i+1);
         if(qCard.size() == 1) break;
+        goBehindNum = qCard.front();
+        qCard.pop();
+        qCard.push(goBehindNum);        
     }
     cout << qCard.front() << endl;
 
