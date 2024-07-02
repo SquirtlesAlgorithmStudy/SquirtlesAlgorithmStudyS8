24e26862eda9a195b6deb2f0d6894462cdc0baa3
euijin-lee <euijin.lee@navercorps.uncomment>
2024-07-02 17:55:19 +0900

    [의진] 싸이버개강총회

---
+++ 의진/240702_bfsdfs_구현/19583_싸이버개강총회.py
@@ -0,0 +1,39 @@
+import sys
+input = sys.stdin.readline
+
+
+def time_to_regular_form(time: str):
+    hour, minute = map(int, time.split(":"))
+    regular_form = hour * 60 + minute
+    return regular_form
+
+
+def log_line_to_info(log_line: str):
+    time, name = log_line.split()
+    time = time_to_regular_form(time)
+    return time, name
+
+
+infos = list(input().rstrip().split())
+S = time_to_regular_form(infos[0])
+E = time_to_regular_form(infos[1])
+Q = time_to_regular_form(infos[2])
+
+# print(S, E, Q)
+
+ans = 0
+enter_check = set()
+while True:
+    try:
+        log_line = input().rstrip()
+        time, name = log_line_to_info(log_line)
+        if time <= S:
+            enter_check.add(name)
+        elif (E <= time <= Q) and (name in enter_check):
+            ans += 1
+            enter_check.remove(name)
+
+    except:
+        break
+
+print(ans)
