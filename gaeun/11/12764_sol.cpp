34e51dd1663c0926663c09f468772df1956f2019
ghan <hge4275@naver.com>
2024-07-16 10:45:22 +0900

    12764 is solved

--- gaeun/11/12764_sol.cpp
+++ gaeun/11/12764_sol.cpp
@@ -19,23 +19,20 @@ struct ascendingSecond{
 int N;
 priority_queue<pair<int, int>, vector<pair<int, int>>, ascendingFirst> startPQ;
 priority_queue<pair<int, int>, vector<pair<int, int>>, ascendingSecond> endPQ;
+priority_queue<int, vector<int>, greater<int>> emptySeatPQ; // min-heap
 vector<int> seat(100000, 0);
-int maxIdx = 0;
+int maxIdx = 1;
 map<pair<int, int>, int> seatMap;
-vector<bool> seatStatus(100000, true);
 int emptySeatIdx;
 
 void findEmptySeat(){
+    if(emptySeatPQ.empty()){
+        emptySeatPQ.push(maxIdx);
+        maxIdx++;
+    } 
     // get the smallest number of empty seat
-    for(int i=0; i<100000; i++){
-        if(seatStatus[i] == true){
-            emptySeatIdx = i;
-            break;
-        }
-    }
-
-    // update the max num of seat
-    if(emptySeatIdx > maxIdx) maxIdx = emptySeatIdx;
+    emptySeatIdx = emptySeatPQ.top();
+    emptySeatPQ.pop();
 }
 
 int main(){
@@ -53,9 +50,8 @@ int main(){
             findEmptySeat();
             seatMap[startPQ.top()] = emptySeatIdx; // map 요소 추가: 1. insert(), 2. [] 연산자
 
-            // 2. 해당 자리에 앉았던 사람 명 수 기억, 해당 자리 occupied 처리
+            // 2. 해당 자리에 앉았던 사람 명 수 기억
             seat[emptySeatIdx]++;
-            seatStatus[emptySeatIdx] = false;
 
             // 3. 변수 업데이트
             startPQ.pop();
@@ -63,14 +59,13 @@ int main(){
         else{
             // 1. 사용기간 끝난 자리 release
             while(startPQ.top().first > endPQ.top().second){
-                seatStatus[seatMap[endPQ.top()]] = true;
+                emptySeatPQ.push(seatMap[endPQ.top()]);
                 endPQ.pop();
             }
 
             // 2. 최소 번호의 자리에 앉히기, 인원 증가, occupied 처리
             findEmptySeat();
             seat[emptySeatIdx]++;
-            seatStatus[emptySeatIdx] = false;
 
             // 4. 앉을 자리 저장
             seatMap[startPQ.top()] = emptySeatIdx;
@@ -80,8 +75,8 @@ int main(){
         }
     }
 
-    cout << maxIdx + 1 << endl;
-    for(int i=0; i<maxIdx + 1; i++){
+    cout << maxIdx-1 << endl;
+    for(int i=1; i<maxIdx; i++){
         cout << seat[i] << " ";
     }
     
