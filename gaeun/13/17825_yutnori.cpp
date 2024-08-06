9b64c9393bdf9a9124f20070a0efc87a368c6f00
ghan <hge4275@naver.com>
2024-08-06 20:01:57 +0900

    17825 is on going

---
+++ gaeun/13/17825_yutnori.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int dice[10]; // 주사위에서 나올 수 10개
+int horse[4]; // 말 4개
+int map[33]; // 윷놀이 판에 존재하는 33개의 node(시작, 도착 포함)
+bool isHorse[33]; // 각 node에 horse 존재 여부
+int score[33]; // 각 node의 점수
+
+void initMap();
+void dfs();
+
+int main(){
+
+}
+
+void initMap(){
+    
+}
