1690431fb0e8a6d368efcbb4c703b9fa02e5947f
psychehose <psychehose@gmail.com>
2024-08-13 19:29:19 +0900

    update

--- hose/12/2352/main.cpp
+++ hose/12/2352/main.cpp
@@ -17,6 +17,8 @@ int main() {
     int n;
     cin >> n;
 
+    vector<int> arr_index;
+
     for(int i = 0; i < n; i++) {
         cin >> arr[i];
     }
@@ -27,14 +29,34 @@ int main() {
     for (int i = 1; i < n; ++i) {
         if (v.back() < arr[i]) {
             v.push_back(arr[i]);
+
         } else {
             auto it = lower_bound(v.begin(), v.end(), arr[i]);
             v[it - v.begin()] = arr[i];
         }
     }
-    
+
     cout << v.size() << '\n';
 
+    for (int i = 0; i < n; i++) {
+        cout << arr_index[i] << ' ';
+    }
 
     return 0;
 }
+
+// lower bound: 찾고자 하는 값 이상이 처음 나오는 위치.
+// 4 2 6 3 1 5
+// 2, 3, 5
+
+// LIS: 4 // index[0] = 1
+// LIS: 2 // index[1] = 1
+// 2 6: //  index[2] = 2
+// 2 3 //  index[3] = 2
+// 1 3 // index[4] = 1
+// 1 3 5 index[5] = 3
+
+
+
+// index[5], index[3], index[1]
+// 5 3 2 뒤집어. 
