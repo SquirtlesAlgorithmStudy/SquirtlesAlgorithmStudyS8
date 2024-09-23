8254c3c1261ad98050ff892d99b99525048f3cf3
psychehose <psychehose@gmail.com>
2024-09-24 00:08:35 +0900

    update

--- hose/src/4485/4485/4485.cpp
+++ hose/src/4485/4485/4485.cpp
@@ -6,6 +6,7 @@
 #include <vector>
 #include <algorithm>
 
+
 using namespace std;
 
 /*
@@ -74,8 +75,8 @@ Problem 3: 36
 #define INT_MAX 2147483647
 
 int N;
-int cost[125][125];
-int dist[125][125] = { INT_MAX, };
+int board[125][125];
+int cost[125][125] = { INT_MAX, };
 bool visited[125][125] = { false, };
 
 int dr[] = { 0, 0, 1, -1 };
@@ -83,18 +84,58 @@ int dc[] = { 1, -1, 0, 0 };
 
 
 
+struct CompareNode {
+	bool operator()(const pair<pair<int, int>, int>& a,
+		const pair<pair<int, int>, int>& b) const {
+		return a.second > b.second;
+	}
+};
+
 
 
 void dijkstra()
 {
     
     // 위치, 비용
-    priority_queue<pair<pair<int, int>, int**>> pq;
+
+	priority_queue<pair<pair<int, int>, int>,
+		vector<pair<pair<int, int>, int>>,
+		CompareNode> pq;
 
 
+    pq.push({ {0, 0}, board[0][0] });
 
+    cost[0][0] = board[0][0];
 
+    while (!pq.empty())
+    {
+		int r = pq.top().first.first;
+		int c = pq.top().first.second;
 
+		int curCost = pq.top().second;
+
+		pq.pop();
+
+		if (visited[r][c])
+			continue;
+
+		visited[r][c] = true;
+
+		for (int i = 0; i < 4; i++)
+		{
+			int nr = r + dr[i];
+			int nc = c + dc[i];
+
+			if (nr < 0 || nc < 0 || nr >= N || nc >= N)
+				continue;
+
+			if (cost[nr][nc] > curCost + board[nr][nc])
+			{
+				cost[nr][nc] = curCost + board[nr][nc];
+				pq.push({ {nr, nc}, cost[nr][nc] });
+			}
+		}
+	}
 
 }
 
@@ -105,6 +146,32 @@ void dijkstra()
 int main()
 {
 
+	int problem = 1;
 
-    std::cout << "Hello World!\n";
+	while (1)
+	{
+		cin >> N;
+
+		if (N == 0)
+			break;
+
+		for (int i = 0; i < N; i++)
+		{
+			for (int j = 0; j < N; j++)
+			{
+				cin >> board[i][j];
+				cost[i][j] = INT_MAX;
+				visited[i][j] = false;
+			}
+		}
+
+		dijkstra();
+
+		cout << "Problem " << problem << ": " << cost[N - 1][N - 1] << endl;
+
+		problem++;
+	}
+
+	return 0;
+
 }
